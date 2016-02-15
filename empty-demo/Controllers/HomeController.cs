b529d1dcc1f11f807284e99f73616de9868cb8ba
kneerunjun <kneerunjun@outlook.com>
2016-02-15 15:13:35 +0530

    creating the empty project chapter1

---
+++ empty-demo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace empty_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Greeting = "We are hostign this rave party @ 6th Avenue , riverside. Get your own hashish!";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RegisterMe() {
+            return View();
+        }
+
+    }
+}
