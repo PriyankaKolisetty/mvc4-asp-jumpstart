47584ed97c1a0ca5d0d796cea8837207772bb873
kneerunjun <kneerunjun@outlook.com>
2016-02-16 12:04:23 +0530

    day01 , some syllabus is pending

---
+++ demo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo.Models;
+
+namespace demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            AppUser user = new AppUser() { alias = "asdsdsdf", email = "dsfdsf", empno = 41993 };
+            ViewBag.Greeting = "This is hello from inside the HomeController";
+
+            return View(user);
+        }
+
+    }
+}
---
+++ demo/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo.Models {
+    public class AppUser {
+        public int empno { get; set; }
+        public string email { get; set; }
+        public string alias { get; set; }
+    }
+}
