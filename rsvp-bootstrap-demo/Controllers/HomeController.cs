af0e3b2e8f2bc53701ac2dfcdd9ba36248c2d270
kneerunjun <kneerunjun@outlook.com>
2016-02-17 10:56:30 +0530

    day02 section 01 , over to hands on

---
+++ rsvp-bootstrap-demo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using rsvp_bootstrap_demo.Models;
+
+namespace rsvp_bootstrap_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(GuestResponse gr) {
+            return View("Thanks");
+        }
+    }
+}
---
+++ rsvp-bootstrap-demo/Models/GuestResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rsvp_bootstrap_demo.Models {
+    public class GuestResponse {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
